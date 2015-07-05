e10c012207d374cd203381e03bfe447d6d0fb5e5
Dogerina <parny_fan2003@doglover.com>
2015-07-05 03:35:41 +0100

    action docs

--- bot/src/main/java/com/inubot/api/oldschool/action/ActionFilter.java
+++ bot/src/main/java/com/inubot/api/oldschool/action/ActionFilter.java
@@ -6,6 +6,9 @@
  */
 package com.inubot.api.oldschool.action;
 
+/**
+ * A filter which allows an {@link com.inubot.api.oldschool.action.tree.Action} to identify itself
+ */
 public interface ActionFilter {
     boolean accept(int opcode, int arg0, int arg1, int arg2);
 }
