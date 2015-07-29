8dc8e2479a2e803e4eea36ece336c6caf2024e89
Dogerina <parny_fan2003@doglover.com>
2015-07-30 00:23:00 +0100

    acces

--- bot/src/main/java/com/inubot/api/util/filter/Filter.java
+++ bot/src/main/java/com/inubot/api/util/filter/Filter.java
@@ -8,25 +8,25 @@ package com.inubot.api.util.filter;
 
 public interface Filter<E> {
 
-    public boolean accept(E e);
+    boolean accept(E e);
 
     /**
-     * @param <T> the type
+     * @param <E> the type
      * @return a Filter that always accepts the element
      */
-    public static <T> Filter<T> always() {
+    static <E> Filter<E> always() {
         return t -> true;
     }
 
     /**
-     * @param <T> the type
+     * @param <E> the type
      * @return a Filter that always rejects the element
      */
-    public static <T> Filter<T> never() {
+    static <E> Filter<E> never() {
         return t -> false;
     }
 
-    public static <E> Filter<E> not(Filter<E> filter) {
+    static <E> Filter<E> not(Filter<E> filter) {
         return e -> !filter.accept(e);
     }
 }
